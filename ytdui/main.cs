7877abdfddf7c10839ccf64512d77013d9110c86
ekoske <ekoske@gmail.com>
2016-04-29 11:38:14 +0200

    Sync Version

---
+++ ytdui/main.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security;
+
+namespace ytdui
+{
+    public partial class main : Form
+    {
+        youtube_dl_wrapper dl = new youtube_dl_wrapper();
+
+        #region Test Kram
+        string[] test_links = new string[] {
+            "https://www.vevo.com/watch/taylor-swift/Shake-It-Off/USCJY1431460",
+            "https://www.vevo.com/watch/taylor-swift/Bad-Blood/USCJY1531563",
+            "https://www.vevo.com/watch/grimes/Kill-V-Maim/GB2871500078",
+            "https://www.vevo.com/watch/grimes/Flesh-without-Blood-Life-in-the-Vivid-Dream/GB2871500053",
+            "https://www.vevo.com/watch/mark-forster/Au-Revoir/DEQ321400099",
+            "https://www.youtube.com/watch?v=IcoqJCJlHbQ" };
+        #endregion
+
+        public main()
+        {
+            InitializeComponent();
+            init_app();
+            init_app_test();
+        }
+
+        private void init_app()
+        {
+            // Load Settings
+            dl.proxy = ytdui.Properties.Settings.Default.proxy;
+            dl.max_threads = Properties.Settings.Default.max_threads;
+            dl.download_folder = Properties.Settings.Default.directory;
+            textBox1.Text = dl.proxy;
+        }
+
+        private void refresh()
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = dl.urls;
+        }
+
+        private void init_app_test()
+        {
+            foreach(string i in test_links) comboBox1.Items.Add(i);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text!="") dl.add(comboBox1.Text);
+            refresh();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dl_item t = (sender as ListBox).SelectedValue as dl_item;
+                listBox2.DataSource = null;
+                listBox2.DataSource = t.output;
+            } catch { }
+            Debug.WriteLine(sender.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            foreach (string i in test_links) dl.add(i);
+            refresh();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dl.proxy=textBox1.Text;
+            ytdui.Properties.Settings.Default.proxy = textBox1.Text;
+            ytdui.Properties.Settings.Default.Save();
+
+        }
+    }
+}
---
+++ ytdui/youtube_dl_wrapper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace ytdui
+{
+    public class youtube_dl_wrapper
+    {
+        public List<dl_item> urls { get; private set; } = new List<dl_item>();
+        public string proxy { get; set; } = "";
+        public int max_threads { get; set; } = 2;
+        public int running_threads { get; private set; } = 0;
+        public string download_folder { get; set; } = "";
+
+        public void add(string url)
+        {
+            Debug.WriteLine(string.Format("Adding '{0}' to downloadlist.", url));
+            dl_item d= new dl_item(url);
+            urls.Add(d);
+            start_download(d);
+            Debug.WriteLine(proxy);
+        }
+
+        public void start_download(dl_item d)
+        {
+            if(running_threads<max_threads)
+            {
+                running_threads++;
+                d.status &= ~dl_state.notstarted;
+                d.status |= dl_state.running;
+                if (download(d) != 0)
+                {
+                    d.status |= dl_state.error;
+                }
+                d.status &= ~dl_state.running;
+                running_threads--;
+            }
+        }
+
+        #region Download not async
+        public int download(dl_item d)
+        {
+            int ret=0;
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "youtube-dl.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            //cmd.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            cmd.StartInfo.UseShellExecute = false;
+            if (proxy=="")
+            {
+                cmd.StartInfo.Arguments = d.url;
+            } else {
+                cmd.StartInfo.Arguments = String.Format("--proxy {0} {1}",proxy,d.url);
+            }
+            cmd.Start();
+            StreamReader sr = cmd.StandardOutput;
+            while (!sr.EndOfStream)
+            {
+                String s = sr.ReadLine();
+                if (s != "")
+                {
+                    Debug.WriteLine(s);
+                    d.output.Add(s);
+                }
+            }
+            sr.ReadToEnd();
+            cmd.WaitForExit();
+            if (cmd.ExitCode != 0) ret = cmd.ExitCode;
+            return ret;
+        }
+        #endregion
+    }
+
+    #region dl_item defintion
+    public class dl_item
+    {
+        public string url;
+        public string filename;
+        public List<string> output;
+        public dl_state status;
+
+        public dl_item(string uri)
+        {
+            url = uri;
+            filename = "";
+            status = dl_state.notstarted;
+            output = new List<string>();
+#if(DEBUG)
+            output.Add(String.Format("Starte youtube-dl für '{0}'...", uri));
+#endif
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}: {1}",status.ToString(),url);
+        }
+    }
+
+    [Flags]
+    public enum dl_state
+    {
+        notstarted = 0x1,
+        running = 0x2,
+        ready = 0x4,
+        error = 0x8
+    }
+    #endregion
+}
